acf8a9b366c2d2fd17d4437269dfb442eee4aa90
Joseph Hale <joseph.hale@rapp.com>
2022-01-12 11:06:41 -0600

    Joe adding mac and linux seperate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo chown ssm ~/.aws
 nohup /usr/bin/sudo /usr/sbin/sshd -D -o ListenAddress=0.0.0.0 &
 instanceId=${1}
 route=${2}
--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # docker build . -t ssm-tunneler
 pip3 install sshuttle
-cp ssm-tunneler-nix /usr/local/bin/ssm-tunneler
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=nix;;
+    Darwin*)    machine=mac;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+cp ssm-tunneler-${machine} /usr/local/bin/ssm-tunneler
